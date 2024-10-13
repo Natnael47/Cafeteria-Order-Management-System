cdff597abd73ab4cc703b355dc82fd1c5e63f303
Natnael47 <nati47m10@gmail.com>
2024-10-13 19:15:43 +0300

    #

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
     return (
         <div className='min-h-screen bg-white border-r'>
             <div className='text-black mt-5'>
-                <NavLink to='/dashboard' className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#DDF7DF] border-r-4 border-primary' : ''}`}>
+                <NavLink to='/' className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#DDF7DF] border-r-4 border-primary' : ''}`}>
                     <img src={assets.home_icon} alt='' />
                     <p className='hidden md:block'>Dashboard</p>
                 </NavLink>
