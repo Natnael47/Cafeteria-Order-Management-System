6b4eb31fc93c767140b803e8dc84865cce2269e5
Natnael47 <nati47m10@gmail.com>
2024-10-29 09:16:28 +0300

    $ prisma user creat token

--- backend/Prisma Controller/PrismaUser.js
+++ backend/Prisma Controller/PrismaUser.js
@@ -6,6 +6,9 @@ import validator from "validator";
 const prisma = new PrismaClient();
 
 const createToken = (id) => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined");
+  }
   return jwt.sign({ id }, process.env.JWT_SECRET);
 };
 
