b8eb3c2b1589d70770e091e680b633bbaf00766d
Natnael47 <nati47m10@gmail.com>
2024-11-11 20:52:37 +0300

    icons

--- chef-waiter/src/Components/Sidebar.jsx
+++ chef-waiter/src/Components/Sidebar.jsx
@@ -42,7 +42,7 @@ const SideBar = () => {
 
                     <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#DDF7DF] border-r-4 border-primary' : ''}`} to={'/add_inventory'}>
                         <img src={assets.people_icon} alt="" />
-                        <p>Inventory</p>
+                        <p>Add Inventory</p>
                     </NavLink>
 
                     <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#DDF7DF] border-r-4 border-primary' : ''}`} to={'/reports'}>
@@ -53,6 +53,11 @@ const SideBar = () => {
                     <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#DDF7DF] border-r-4 border-primary' : ''}`} to={'/suppliers'}>
                         <img src={assets.people_icon} alt="" />
                         <p>Suppliers</p>
+                    </NavLink>
+
+                    <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#DDF7DF] border-r-4 border-primary' : ''}`} to={'/inventory'}>
+                        <img src={assets.home_icon} alt="" />
+                        <p>Inventory</p>
                     </NavLink>
 
                     <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#DDF7DF] border-r-4 border-primary' : ''}`} to={'/inv-orders'}>
--- chef-waiter/src/assets/assets.js
+++ chef-waiter/src/assets/assets.js
@@ -3,6 +3,7 @@ import chef_icon from "./chef_icon.png";
 import home_icon from "./home_icon.png";
 import logo from "./logo.png";
 import logo2 from "./logo2.png";
+import modify_icon from "./modify.png";
 import order_icon from "./order_icon.png";
 import parcel_icon from "./parcel_icon.png";
 import profile_image from "./profile_image.png";
@@ -22,6 +23,8 @@ export const assets = {
   report_icon,
   home_icon,
   chef_icon,
+  trash_icon,
+  modify_icon,
 };
 
 //export const url = 'http://localhost:4000'
