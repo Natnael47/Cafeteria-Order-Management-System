7f732d948fa9b0efed00326e4a35e1f9c80f8a65
Natnael47 <nati47m10@gmail.com>
2024-11-13 20:13:40 +0300

    # move add inventory function to context

--- chef-waiter/src/Pages/Inventory_manager/AddInventory.jsx
+++ chef-waiter/src/Pages/Inventory_manager/AddInventory.jsx
@@ -43,61 +43,72 @@ const AddInventory = () => {
                 </div>
                 <div className="w-full">
                     <p className="mb-2 mt-4">Item Name</p>
-                    <input className="w-full max-w-[500px] px-3 py-2" onChange={onChangeHandler} value={data.name} type='text' name='name' placeholder='Write Name' required />
+                    <input className="w-full max-w-[500px] px-3 py-2 border border-gray-600 rounded-md focus:border-black" onChange={onChangeHandler} value={data.name} type='text' name='name' placeholder='Write Name' required />
                 </div>
                 <div className="w-full">
                     <p className="mb-2 mt-4">Description</p>
-                    <textarea className="w-full max-w-[500px] max-h-[100px] px-3 py-2" onChange={onChangeHandler} value={data.description} name='description' rows="6" placeholder='Write Description Here' required />
+                    <textarea className="w-full max-w-[500px] max-h-[100px] px-3 py-2 border border-gray-600 rounded-md focus:border-black" onChange={onChangeHandler} value={data.description} name='description' rows="6" placeholder='Write Description Here' required />
                 </div>
-                <div>
-                    <p className="mb-2">Category</p>
-                    <select className="w-full px-3 py-2 sm:w-[120px] relative max-h-48 overflow-y-auto" onChange={onChangeHandler} value={data.category} name='category'>
-                        <option value="Electronics">Electronics</option>
-                        <option value="Furniture">Furniture</option>
-                        <option value="Stationery">Stationery</option>
-                        <option value="Cleaning Supplies">Cleaning Supplies</option>
-                        <option value="Food Supplies">Food Supplies</option>
-                    </select>
-                </div>
-                <div className="flex flex-col sm:flex-row gap-2 w-full sm:gap-8 mt-3">
+                <div className="flex flex-row sm:flex-row gap-2 w-full sm:gap-8">
                     <div>
-                        <p className="mb-2">Quantity</p>
-                        <input className="w-full px-3 py-2 sm:w-[120px]" onChange={onChangeHandler} value={data.quantity} type='number' name='quantity' placeholder='0' required />
+                        <p className="mb-2 mt-4">Category</p>
+                        <select
+                            className="w-full max-w-[200px] px-3 py-2 border border-gray-600 rounded-md focus:border-black"
+                            onChange={onChangeHandler}
+                            value={data.category}
+                            name="category"
+                        >
+                            <option value="Electronics">Electronics</option>
+                            <option value="Furniture">Furniture</option>
+                            <option value="Stationery">Stationery</option>
+                            <option value="Cleaning Supplies">Cleaning Supplies</option>
+                            <option value="Food Supplies">Food Supplies</option>
+                        </select>
                     </div>
                     <div>
+                        <p className="mb-2 mt-4">Quantity</p>
+                        <input className="w-full px-3 py-2 sm:w-[150px] border border-gray-600 rounded-md focus:border-black" onChange={onChangeHandler} value={data.quantity} type='number' name='quantity' placeholder='0' required />
+                    </div>
+                </div>
+                <div className="flex flex-col sm:flex-row gap-2 w-full sm:gap-8 mt-3">
+
+                    <div>
                         <p className="mb-2">Unit</p>
-                        <input className="w-full px-3 py-2 sm:w-[120px]" onChange={onChangeHandler} value={data.unit} type='text' name='unit' placeholder='kg, pcs, etc.' required />
+                        <input className="w-full px-3 py-2 sm:w-[150px] border border-gray-600 rounded-md focus:border-black" onChange={onChangeHandler} value={data.unit} type='text' name='unit' placeholder='kg, pcs, etc.' required />
                     </div>
                     <div>
                         <p className="mb-2">Price Per Unit</p>
-                        <input className="w-full px-3 py-2 sm:w-[120px]" onChange={onChangeHandler} value={data.pricePerUnit} type='number' name='pricePerUnit' placeholder='$' required />
+                        <input className="w-full px-3 py-2 sm:w-[150px] border border-gray-600 rounded-md focus:border-black" onChange={onChangeHandler} value={data.pricePerUnit} type='number' name='pricePerUnit' placeholder='$' required />
                     </div>
                     <div>
                         <p className="mb-2">Status</p>
-                        <input className="w-full px-3 py-2 sm:w-[120px]" onChange={onChangeHandler} value={data.status} type='text' name='status' placeholder='Available, Pending, etc.' required />
+                        <input className="w-full px-3 py-2 sm:w-[150px] border border-gray-600 rounded-md focus:border-black" onChange={onChangeHandler} value={data.status} type='text' name='status' placeholder='Available, Pending, etc.' required />
                     </div>
                 </div>
+
                 <div className="flex flex-col sm:flex-row gap-2 w-full sm:gap-8 mt-3">
                     <div>
                         <p className="mb-2">Date Received</p>
-                        <input className="w-full px-3 py-2 sm:w-[120px]" onChange={onChangeHandler} value={data.dateReceived} type='date' name='dateReceived' required />
+                        <input className="w-full px-3 py-2 sm:w-[150px] border border-gray-600 rounded-md focus:border-black" onChange={onChangeHandler} value={data.dateReceived} type='date' name='dateReceived' required />
                     </div>
                     <div>
                         <p className="mb-2">Supplier</p>
-                        <input className="w-full px-3 py-2 sm:w-[120px]" onChange={onChangeHandler} value={data.supplier} type='text' name='supplier' required />
+                        <input className="w-full px-3 py-2 sm:w-[150px] border border-gray-600 rounded-md focus:border-black" onChange={onChangeHandler} value={data.supplier} type='text' name='supplier' required />
                     </div>
                     <div>
                         <p className="mb-2">Expiry Date</p>
-                        <input className="w-full px-3 py-2 sm:w-[120px]" onChange={onChangeHandler} value={data.expiryDate} type='date' name='expiryDate' required />
+                        <input className="w-full px-3 py-2 sm:w-[150px] border border-gray-600 rounded-md focus:border-black" onChange={onChangeHandler} value={data.expiryDate} type='date' name='expiryDate' required />
                     </div>
                 </div>
-                <div className="flex w-full justify-center gap-3 mt-8">
+
+                <div className="flex w-full justify-start gap-3 mt-8">
                     <button type="submit" className="px-8 py-2 rounded text-white bg-[#0b5ed7]">
                         Submit
                     </button>
                 </div>
             </div>
         </form>
+
     );
 }
 
