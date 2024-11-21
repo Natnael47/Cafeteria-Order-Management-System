81f7257f40d9474b70404fc6eae4f2746c51173c
Natnael47 <nati47m10@gmail.com>
2024-11-21 18:55:24 +0300

    update INV

--- chef-waiter/src/Pages/Inventory_manager/Inventory.jsx
+++ chef-waiter/src/Pages/Inventory_manager/Inventory.jsx
@@ -210,11 +210,11 @@ const Inventory = () => {
             </div>
             <div className="bg-[#F3F4F6] rounded w-full max-w-5.3xl max-h-[88vh] overflow-scroll">
                 <div>
-                    <div className="grid grid-cols-[0.7fr_0.7fr_0.7fr_0.6fr_0.5fr_0.5fr_0.5fr] items-center gap-2 p-3 border text-sm font-medium bg-[#FAFAFA] text-black sm:grid">
+                    <div className="grid grid-cols-[0.4fr_0.8fr_0.7fr_0.6fr_0.5fr_0.5fr_0.5fr] items-center gap-2 p-3 border text-sm font-medium bg-[#FAFAFA] text-black sm:grid">
+                        <b>Name</b>
                         <b>Status</b>
-                        <b className="ml-5">Name</b>
+                        <b className="ml-2">Quantity</b>
                         <b>Category</b>
-                        <b>Quantity</b>
                         <b>Price / Unit</b>
                         <b>Remove</b>
                         <b>Modify</b>
@@ -222,8 +222,12 @@ const Inventory = () => {
 
                     {filteredAndSortedInventoryList.map((item, index) => (
                         <div key={index}>
-                            <div className={`grid grid-cols-[0.7fr_0.7fr_0.7fr_0.6fr_0.5fr_0.5fr_0.5fr] items-center gap-2 p-3 border text-sm font-medium sm:grid ${item.status === "out of stock" ? "bg-red-100" : "bg-white"}`}>
-                                <div className="relative w-full bg-gray-200 rounded h-8">
+                            <div className={`grid grid-cols-[0.4fr_0.8fr_0.7fr_0.6fr_0.5fr_0.5fr_0.5fr] items-center gap-2 p-3 border text-sm font-medium sm:grid ${item.status === "out of stock" ? "bg-red-100" : "bg-white"}`}>
+
+                                <p className="text-[#112F45] cursor-pointer hover:text-blue-500 hover:font-bold" onClick={() => handleNameClick(index)}>
+                                    {item.name}
+                                </p>
+                                <div className="relative w-[200px] bg-gray-200 rounded h-8">
                                     <div
                                         className={`absolute top-0 left-0 h-8 rounded transition-all`}
                                         style={{
@@ -235,13 +239,8 @@ const Inventory = () => {
                                         {item.status}%
                                     </p>
                                 </div>
-
-
-                                <p className="text-[#112F45] cursor-pointer hover:text-blue-500 ml-5" onClick={() => handleNameClick(index)}>
-                                    {item.name}
-                                </p>
+                                <p className="ml-2">{item.quantity} {item.unit}</p>
                                 <p>{item.category}</p>
-                                <p>{item.quantity} {item.unit}</p>
                                 <p>${item.pricePerUnit}</p>
                                 <p onClick={() => openModal(item.id)} className="cursor-pointer pl-1">
                                     <img src={assets.trash_icon} alt="Remove" className="w-7 ml-2 hover:scale-125 transition-transform" />
